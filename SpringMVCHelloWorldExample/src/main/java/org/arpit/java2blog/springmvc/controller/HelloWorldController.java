0ba1bade3a2fc5de683d0899d646a3b2ce3999e4
arpitmandliya <mandliya@gmail.com>
2015-09-13 22:54:44 +0530

    Spring MVC hello world example
    Change-Id: Id61798ab87684b7c09af08e208c52531cdc4fdd8
    Signed-off-by: arpitmandliya <mandliya@gmail.com>

---
+++ SpringMVCHelloWorldExample/src/main/java/org/arpit/java2blog/springmvc/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+
+package org.arpit.java2blog.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+ 
+ @RequestMapping("/helloworld")
+ public ModelAndView hello() {
+ 
+  String helloWorldMessage = "Hello world from java2blog!";
+  return new ModelAndView("hello", "message", helloWorldMessage);
+ }
+}
+
+
